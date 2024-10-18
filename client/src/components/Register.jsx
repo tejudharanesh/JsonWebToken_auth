18ad86dc0c327390715b2527f8f05741cdca60b7
tejudharanesh <tejudharanesh1234@gmail.com>
2024-10-18 16:54:41 +0530

    initial commit

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("http://localhost:5000/api/auth/register", {
-      username,
-      password,
-    });
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/register",
+      {
+        username,
+        password,
+      }
+    );
+    console.log(response);
+
+    if (response.status === 201) {
+      toast("Registration failed");
+    } else {
+      toast("registration Success !");
+      // navigate("/login");
+    }
   };
 
   return (
